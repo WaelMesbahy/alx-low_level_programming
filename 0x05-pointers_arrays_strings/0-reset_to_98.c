3389f12463bc70bdb61ee6d39a8b959e115f4fa6
WaelMesbahy <waelmesbahy@gmail.com>
2023-07-24 21:00:47 +0100

    puch yourself

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+* reset_to_98 - a function that modify the value of an integer that
+* is refered to a pointer
+*
+* @n : the integer
+*/
+void reset_to_98(int *n);
+{
+*n = 98;
+}
